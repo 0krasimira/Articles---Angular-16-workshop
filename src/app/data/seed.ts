b93ea3ac77f8d9797d948e9febfde7a6b3f80704
krasimira.stoyanova <134604658+0krasimira@users.noreply.github.com>
2024-02-29 14:12:38 +0200

    Add files via upload

---
+++ src/app/data/seed.ts
@@ -0,0 +1,31 @@
+const data = [
+    {
+        title: 'Best summer vacation places',
+        description: 'Plan a perfect getaway in a place where the sun shines all year. Explore the world\'s most pristine beaches, enjoy relaxed activities, and pamper yourself at the many wellness retreats. Check these amazing places and decide where your next vacation will be! Good luck',
+        author: 'Vacation Vacationov',
+        imageUrl: 'https://media.easemytrip.com/media/Blog/International/637867579316652136/637867579316652136hNPxy2.jpg'
+    },
+
+    {
+        title: '10 best stocks to invest in 2024',
+        description: 'Read The Full Report — Discover stock opportunities for 2024. Read the full report. Capital at risk. Stock Market is ever changing and you should be aware with where you put your money! Invest at your own risk',
+        author: 'Stock Stockov',
+        imageUrl: 'https://stoxbox.in/wp-content/uploads/2023/11/10-stocks-to-invest-in-2024.jpg'
+    },
+
+    {
+        title: 'Everything you need to know about astrology',
+        description: 'Become the next big astrologer by learning from the most renouned names in the industry. Join a team of professionals and have fun while learning.',
+        author: 'Astrologer Astrologerov',
+        imageUrl: 'https://cdn.britannica.com/72/223172-131-C3F72804/astrology-horoscope-circle.jpg'
+    },
+
+    {
+        title: 'Most captivating archaeological discoveries of the 20th century',
+        description: 'Archaeological discoveries unravel secrets and challenge our ideas about human history. In this article, you can check the 20th century discoveries that do that and more.',
+        author: 'Archaeologis Archaeologistov',
+        imageUrl: 'https://deih43ym53wif.cloudfront.net/temple-pharaoh-rameses-abu-simbel-egypt-aswan-shutterstock_1681467283.jpg_5722520688.jpg'
+    },
+]
+
+export {data}
